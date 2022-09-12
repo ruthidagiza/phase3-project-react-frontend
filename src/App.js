1ceb7daba1e7c87de7ea49aa5d912093ed47c91f
ruthidagiza <ruthidagiza@gmail.com>
2022-09-13 01:03:30 +0300

    added react router

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <div className="flex flex-col w-full bg-gray-200 min-h-screen ">
+        <nav>
+          <ul className="flex space-x-2">
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" component={Home} />
+          <Route path="/login" component={Login} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+function Login() {
+  return (
+    <div className="flex flex-col">
+      <h2 className="text-3xl">Login</h2>;
     </div>
   );
+}
+
+function Home() {
+  return <h2 className="text-3xl">Home</h2>;
 }
 
 export default App;
