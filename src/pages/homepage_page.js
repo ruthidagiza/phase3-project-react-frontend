41ec4fe2bc10b7ba0b277320c681221f6e4894dc
ruthidagiza <ruthidagiza@gmail.com>
2022-09-14 07:38:25 +0300

    fixed fectching articles on page load wituseeffect

--- src/pages/homepage_page.js
+++ src/pages/homepage_page.js
@@ -1,11 +1,12 @@
 import axios from "axios";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import ArticleComponent from "../components/article_component";
 import { productionUrl } from "../models/constants";
 
 function Homepage() {
   const [artcles, setArtcles] = useState([]);
+  const [loading, setLoading] = useState(false);
   const fetchArticlesFromBackend = async () => {
     try {
       let res = await axios.get(`${productionUrl}/articles`);
@@ -14,13 +15,29 @@ function Homepage() {
       console.log(e);
     }
   };
+  useEffect(() => {
+    fetchArticlesFromBackend();
+  }, []);
   return (
     <div className="flex flex-col w-full">
-      {Array(4)
-        .fill(1)
-        .map((e, i) => (
-          <ArticleComponent key={i} />
+      {!loading ||
+        (artcles.length < 1 && (
+          <div className="flex flex-col justify-center items-center w-full h-[60vh]">
+            {!loading && (
+              <p className="text-sm text-teal-700">Loading Articles ...</p>
+            )}
+            {artcles.length < 1 && (
+              <p className="text-sm text-red-500">
+                No articles saved in the server
+              </p>
+            )}
+          </div>
         ))}
+
+      {!loading &&
+        Array(2)
+          .fill(1)
+          .map((e, i) => <ArticleComponent key={i} />)}
     </div>
   );
 }
